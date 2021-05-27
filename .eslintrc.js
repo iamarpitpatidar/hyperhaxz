8d56070218293d1476256e46e7af2d0fe01fb8d8
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-27 23:29:56 +0530

    init

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+	root: true,
+	"env": {
+		"browser": true,
+		"es6": true,
+		"node": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"warn",
+			"tab"
+		],
+		"quotes": [
+			"warn",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-var": [
+			"error"
+		],
+		"no-console": [
+			"off"
+		],
+		"no-unused-vars": [
+			"off"
+		],
+		"no-mixed-spaces-and-tabs": [
+			"warn"
+		]
+	}
+};
