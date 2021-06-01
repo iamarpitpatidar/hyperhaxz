7a3f0ca10e7a006680320e7ab3084cd27191c12b
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-01 14:22:41 +0530

    new: added login and signup page

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,7 +3,21 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Home'
+  })
+})
+
+router.get('/auth/login', (req, res) => {
+  res.render('auth/login', {
+    title: 'Login'
+  })
+})
+
+router.get('/auth/register', (req, res) => {
+  res.render('auth/register', {
+    title: 'Sign Up'
+  })
 })
 
 export default router
