a2001eca55842dff33014eed4116746782027eda
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-01 20:17:42 +0530

    feat: added client-side form validation logic in login page

--- src/app/public/scripts/login.js
+++ src/app/public/scripts/login.js
@@ -1,1 +1,55 @@
-console.log('Yay! I am loaded')
+function loginData () {
+  return {
+    user: {
+      username: {
+        hasVal: false,
+        $val: '',
+        $error: false
+      },
+      password: {
+        hasVal: false,
+        $val: '',
+        $error: false
+      },
+      isValid: false
+    },
+    submit: function () {
+      this.input('username')
+      this.input('password')
+      this.validate()
+
+      if (this.user.isValid === true) {
+        loginUser(`${this.user.username.$val}:${this.user.password.$val}`)
+      }
+    },
+    validate: function () {
+      if (this.user.username.hasVal) {
+        const username = String(this.user.username.$val)
+        this.user.username.$error = !username ? 'Please enter your username' : ''
+      }
+      if (this.user.password.hasVal) {
+        const password = String(this.user.password.$val)
+        this.user.password.$error = !password ? 'Please enter your password' : ''
+      }
+      this.user.isValid = !this.user.username.$error && !this.user.password.$error
+    },
+    input: function (name) {
+      if (['username', 'password'].includes(name)) this.user[name].hasVal = true
+      this.validate()
+    }
+  }
+}
+
+function loginUser (data) {
+  axios({
+    method: 'post',
+    url: '/auth/login',
+    credentials: 'same-origin',
+    headers: {
+      Authorization: `Basic ${btoa(data)}`,
+      Accept: 'application/json',
+      'XSRF-Token': document.querySelector('meta[name="session-identifier"]').getAttribute('content'),
+      'Content-Type': 'application/json'
+    }
+  })
+}
--- src/app/routes.js
+++ src/app/routes.js
@@ -10,14 +10,22 @@ router.get('/', (req, res) => {
 
 router.get('/auth/login', (req, res) => {
   res.render('auth/login', {
-    title: 'Login'
+    title: 'Login',
+    csrfToken: req.csrfToken()
   })
+})
+router.post('/auth/login', (req, res) => {
+  res.json(req.headers)
 })
 
 router.get('/auth/register', (req, res) => {
   res.render('auth/register', {
-    title: 'Sign Up'
+    title: 'Sign Up',
+    csrfToken: req.csrfToken()
   })
+})
+router.post('/auth/register', (req, res) => {
+  console.log(req.body)
 })
 
 export default router
