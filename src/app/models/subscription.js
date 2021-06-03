f61e224dd845e45c3d651ffdbe1c521d4ea5481f
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-03 20:54:34 +0530

    new: create subscription after user is created

---
+++ src/app/models/subscription.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from 'mongoose'
+
+const subscriptionSchema = new Schema({
+  activationKeyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    unique: true,
+    index: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  expiry: {
+    type: Date,
+    required: true,
+    default: Date.now() + (24 * 60 * 60 * 1000)
+  },
+  createdBy: {
+    type: String,
+    default: 'admin'
+  }
+}, {
+  timestamps: true
+})
+
+const model = mongoose.model('Subscription', subscriptionSchema)
+
+export const schema = model.schema
+export default model
