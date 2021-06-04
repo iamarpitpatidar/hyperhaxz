b4acf9f9df9ea0b545a16c70950567559436eefe
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-04 14:05:10 +0530

    added users to express locals

--- src/app/helper.js
+++ src/app/helper.js
@@ -1,4 +1,12 @@
-export const checkAuth = (req, res, next) => {
+export const checkUser = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user
+    ;['secret', 'keywords', 'password', '__v'].forEach(each => delete res.locals.user[each])
+  }
+  return next()
+}
+
+export const requireAuth = (req, res, next) => {
   if (req.isAuthenticated()) return next()
   else res.redirect('/auth/login')
 }
--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,12 +2,13 @@ import { Router } from 'express'
 import main from './main'
 import auth from './auth'
 import dashboard from './dashboard'
-import { checkAuth } from '../helper'
+import { checkUser, requireAuth } from '../helper'
 
 const router = Router()
 
+router.use(checkUser)
 router.use('/', main)
 router.use('/auth', auth)
-router.use('/dashboard', checkAuth, dashboard)
+router.use('/dashboard', requireAuth, dashboard)
 
 export default router
