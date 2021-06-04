cb3eb4c91abc0a66d9c60c08738b73cb6f8a5b44
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-04 10:54:35 +0530

    new: added dashboard route

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import main from './main'
 import auth from './auth'
+import dashboard from './dashboard'
+import { checkAuth } from '../helper'
 
 const router = Router()
 
 router.use('/', main)
 router.use('/auth', auth)
+router.use('/dashboard', checkAuth, dashboard)
 
 export default router
