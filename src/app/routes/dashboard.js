6a96d3f3ab3c8d70ec062205f37da17f700bac8b
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-04 21:51:57 +0530

    new: added profile and subscription route

--- src/app/routes/dashboard.js
+++ src/app/routes/dashboard.js
@@ -3,7 +3,19 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.render('dashboard')
+  res.render('dashboard', {
+    title: 'Dashboard'
+  })
+})
+router.get('/profile', (req, res) => {
+  res.render('dashboard/profile', {
+    title: 'Profile'
+  })
+})
+router.get('/subscriptions', (req, res) => {
+  res.render('dashboard/subscriptions', {
+    title: 'Subscriptions'
+  })
 })
 
 export default router
