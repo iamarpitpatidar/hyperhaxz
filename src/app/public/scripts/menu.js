6e55761473d04355924e38bbb906afd235e3036e
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-04 22:36:18 +0530

    feat: handling menu toggles

--- src/app/public/scripts/menu.js
+++ src/app/public/scripts/menu.js
@@ -1,8 +1,24 @@
 function menu () {
   return {
     isMenuOpen: false,
-    toggleMenu: function () {
-      this.isMenuOpen = !this.isMenuOpen
+    closeMenu: function () {
+      this.isMenuOpen = false
+    },
+    openMenu: function () {
+      this.isMenuOpen = true
+    },
+    handleClick: function () {
+      if (this.isMenuOpen) this.closeMenu()
+      else this.openMenu()
+    },
+    init () {
+      window.addEventListener('mousedown', event => {
+        const target = event.target
+        const active = document.activeElement
+
+        if (active.contains(target)) return
+        if (this.isMenuOpen && document.getElementById('main').contains(target)) this.handleClick()
+      })
     }
   }
 }
