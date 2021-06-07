486ab0e55d5af5319fd73abd3f38e686528363b7
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-07 22:24:08 +0530

    added protected roles middleware

--- src/app/helper.js
+++ src/app/helper.js
@@ -10,3 +10,9 @@ export const requireAuth = (req, res, next) => {
   if (req.isAuthenticated()) return next()
   else res.redirect('/auth/login')
 }
+
+export const allowRoles = (roles) => (req, res, next) => {
+  if (!Array.isArray(roles)) return res.sendStatus(404)
+  if (roles.includes(req.user.role)) next()
+  else res.sendStatus(404)
+}
