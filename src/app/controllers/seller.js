bfbbb03fe5f823957e6e89a001a664ab0fe5e618
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-10 22:48:12 +0530

    added sellers logic

--- src/app/controllers/seller.js
+++ src/app/controllers/seller.js
@@ -1,3 +1,14 @@
-export const index = (req, res, next) => {
+import User from '../models/user'
+
+export const index = async ({ querymen: { query, select, cursor }, user }, res, next) => {
+  query.role = 'seller'
+  await User.find(query, select, cursor)
+    .then((sellers = []) => sellers.map(each => each.view(user.role)))
+    .then(sellers => {
+      res.locals.sellers = sellers
+      console.log(res.locals.sellers)
+    })
+    .catch(res.status(500))
+
   next()
 }
