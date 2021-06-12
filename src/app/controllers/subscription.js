7ca04252b9ed792d3ae2edc469db4ded1a88f33e
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-12 14:37:39 +0530

    added logic for adding subscription with active roles

--- src/app/controllers/subscription.js
+++ src/app/controllers/subscription.js
@@ -4,7 +4,7 @@ import { error } from '../services/response'
 import logger from '../../core/logger'
 
 export const index = async ({ user }, res, next) => {
-  await Subscription.find({ createdBy: user._id, expiry: { $gt: Date.now() } }).sort({ expiry: -1 })
+  await Subscription.find({ createdBy: user._id, expiry: { $gt: Date.now() } }).sort({ expiry: 1 })
     .then((subscriptions = []) => subscriptions.map(each => each.view()))
     .then(subscriptions => {
       res.locals.subscriptions = subscriptions
@@ -13,12 +13,21 @@ export const index = async ({ user }, res, next) => {
   next()
 }
 
-export const create = ({ bodymen: { body }, user }, res) => {
+export const create = ({ bodymen: { body }, user }, res, next) => {
   Invite.findOne({ code: body.activationKey.toLowerCase() })
-    .then(key => {
+    .then(async key => {
       if (!key) return error(res, 'Activation Key is invalid', 422)
       if (key.used) return error(res, 'Activation Key has already been used!', 422)
 
+      await Subscription.findOne({ createdBy: user._id, role: key.role, expiry: { $gt: Date.now() } })
+        .then(subscription => {
+          if (!!subscription) {
+            subscription.expiry = Date.now()
+            subscription.save().then(sub => logger.info(`Subscription with id: ${sub._id} manually set to expired.`))
+          }
+        })
+      return key
+    }).then(key => {
       Subscription.create({
         activationKeyId: key._id,
         role: key.role,
@@ -29,7 +38,7 @@ export const create = ({ bodymen: { body }, user }, res) => {
         res.status(201).json({ status: 'ok', message: 'Subscription has been added successfully created' })
       }).then(() => {
         key.used = true
-        return key.save().then(() => logger.info(`ActivationKey used by ${body.username} with id: ${key._id}`))
+        return key.save().then(() => logger.info(`ActivationKey used by ${user.username} with id: ${key._id}`))
       }).catch(err => {
         if (err.name === 'MongoError' && err.code === 11000) error(res, 'Oops! Subscription already exists', 409)
         else error(res, 500)
