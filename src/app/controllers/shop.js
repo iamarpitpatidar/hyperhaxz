9772f6922430c9ef7e30cc3eaa452c25a168149f
Hyper AWS <hyper@arpitpatidar.com>
2021-07-11 12:42:06 +0530

    shop design mock

---
+++ src/app/controllers/shop.js
@@ -0,0 +1,13 @@
+import Product from '../models/product'
+
+export const index = (seller) => ({ querymen: { query, select, cursor } }, res, next) => {
+  // query.isSeller = seller
+  Product.countDocuments(query)
+    .then(count => Product.find(query, select, cursor)
+      .then(products => {
+        res.locals.products = products
+        res.locals.count = count
+        res.locals.cursor = cursor
+      })
+      .then(next))
+}
--- src/app/routes/main.js
+++ src/app/routes/main.js
@@ -1,11 +1,29 @@
 import { Router } from 'express'
+import { index } from '../controllers/shop'
+import { middleware as query } from 'querymen'
+import { parseQuery } from '../helper'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Home'
+router.get('/',
+  query(),
+  index(false),
+  (req, res) => {
+    res.render('index', {
+      title: 'Home',
+      plugins: {
+        search: 'Search Products...',
+        query: parseQuery(req.originalUrl),
+        sort: {
+          title: 'sort products',
+          options: {
+            Default: '',
+            Price: 'price'
+          }
+        }
+      },
+      csrfToken: req.csrfToken()
+    })
   })
-})
 
 export default router
