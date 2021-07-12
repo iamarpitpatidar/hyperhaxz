5e23d1da97a4f90648c53e342db326c27f3b32b5
Hyper AWS <hyper@arpitpatidar.com>
2021-07-12 07:21:11 +0530

    added working shop modal

--- src/app/public/scripts/shop.js
+++ src/app/public/scripts/shop.js
@@ -2,19 +2,26 @@ function shop () {
   return {
     data: {
       product: {
-        _id: null,
-        sellix: null
+        _id: null
       }
     },
     open (_id) {
       const dataset = JSON.parse(document.getElementById(_id).dataset.modal)
       this.data.product.sellix = dataset
-      let options = ''
+      let options = '<option value="" disabled selected></option>'
       dataset.forEach(each => {
         options += `<option value="${each.length}">${each.length}</option>`
       })
       document.getElementById('length').innerHTML = options
       document.getElementById('modal').classList.remove('hidden')
+    },
+    update () {
+      const length = document.getElementById('length').value
+      const currentProject = this.data.product.sellix.filter(each => Number(each.length) === Number(length))[0]
+      const quantity = document.getElementById('quantity').value
+
+      document.getElementById('_id').value = currentProject ? currentProject._id : ''
+      document.getElementById('total').innerText = currentProject ? `$ ${currentProject.price * quantity}` : '$ 0.0'
     },
     close () {
       document.getElementById('modal').classList.add('hidden')
