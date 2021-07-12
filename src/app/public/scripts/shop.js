e6849a49b85a1e8a4a3873cec62e2094d5dd8d4f
Hyper AWS <hyper@arpitpatidar.com>
2021-07-12 08:03:02 +0530

    added payment gateways to modal

--- src/app/public/scripts/shop.js
+++ src/app/public/scripts/shop.js
@@ -17,11 +17,61 @@ function shop () {
     },
     update () {
       const length = document.getElementById('length').value
-      const currentProject = this.data.product.sellix.filter(each => Number(each.length) === Number(length))[0]
+      const currentProduct = this.data.product.sellix.filter(each => Number(each.length) === Number(length))[0]
       const quantity = document.getElementById('quantity').value
+      let gateways = ''
+      const gatewayData = {
+        FREE: {
+          name: 'Free',
+          icon: ''
+        },
+        PAYPAL: {
+          name: 'Paypal',
+          icon: ''
+        },
+        BITCOIN: {
+          name: 'Bitcoin',
+          icon: ''
+        },
+        ETHEREUM: {
+          name: 'Ethereum',
+          icon: ''
+        },
+        LITECOIN: {
+          name: 'Litecoin',
+          icon: ''
+        },
+        PERFECT_MONEY: {
+          name: 'Perfect Money',
+          icon: ''
+        },
+        BITCOIN_CASH: {
+          name: 'Bitcoin Cash',
+          icon: ''
+        },
+        SKRILL: {
+          name: 'Skrill',
+          icon: ''
+        },
+        STRIPE: {
+          name: 'Stripe',
+          icon: ''
+        },
+        CASH_APP: {
+          name: 'Cash App',
+          icon: ''
+        }
+      }
 
-      document.getElementById('_id').value = currentProject ? currentProject._id : ''
-      document.getElementById('total').innerText = currentProject ? `$ ${currentProject.price * quantity}` : '$ 0.0'
+      currentProduct.gateways.forEach(each => {
+        gateways += `<label class="cursor-pointer">
+                      <input type="radio" class="hidden" name="gateway">
+                      <div class="label w-full border-blueGray-700 bg-blueGray-700 appearance-none rounded-md px-3 py-2 border-2 text-gray-300 focus:outline-none sm:text-sm">${gatewayData[each].name}</div>
+                  </label>`
+      })
+      document.getElementById('payment_gateways').innerHTML = gateways
+      document.getElementById('_id').value = currentProduct ? currentProduct._id : ''
+      document.getElementById('total').innerText = currentProduct ? `$ ${currentProduct.price * quantity}` : '$ 0.0'
     },
     close () {
       document.getElementById('modal').classList.add('hidden')
